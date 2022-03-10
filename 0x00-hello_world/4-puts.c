f6175418d965204736552b82c09e5c3f92180949
ritawekesa <wekesaritah1@gmail>
2022-03-10 13:21:31 -0800

    puts .c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  *  * main-print the phrase "Programming is like building a multilingual puzzle
  *  * Return: 0 if exited properly,non-zero otherwise
- *  */
+**/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
