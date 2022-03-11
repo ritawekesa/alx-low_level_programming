e75a92c6148cafbb5bd18468de48c4197d2fc22c
ritawekesa <wekesaritah1@gmail>
2022-03-11 12:44:51 -0800

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,13 @@ int main(void)
 		char ch = 'a';
 
 		while (ch <= 'z')
-		{             
+		     {             
 			if (ch != 'e' && ch != 'q')
-		        {
-				putchar(ch);
-		        }
+		         	{
+					putchar(ch);
+		                 }  	
 				ch++;
-		}	
-	        putchar('\n');
+		     }	
+	        	putchar('\n');
 		return (0);
 }
