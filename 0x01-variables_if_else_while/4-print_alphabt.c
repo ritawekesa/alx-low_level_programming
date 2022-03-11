6b44655aa36e764eb643f065cb5cd58bc07191be
ritawekesa <wekesaritah1@gmail>
2022-03-11 14:38:55 -0800

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,18 +5,19 @@
  *   *   * followed by a new line, except q and e
  *    *    * Return: Aways 0 (Sucess)
 **/
-int main(void) 
+int main(void)
 {
-	  char ch = 'a';
-
-	  while (ch <= 'z') 
-	  {
-         	if (ch != 'e' && ch != 'q') 
+	
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
 		{
-		    putchar(ch);
-   		}
-	        ch++;
-          }
-	  putchar('\n');
-	  return (0);
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
