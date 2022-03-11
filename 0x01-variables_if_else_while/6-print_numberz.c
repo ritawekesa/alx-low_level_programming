c3519f3d7557d19972e6d8e2d55aadb44e8c5e97
ritawekesa <wekesaritah1@gmail>
2022-03-11 12:07:32 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,12 @@ int main(void)
 {
 		int n;
 
-			for (n = 48; n < 58; n++)
-			{
+		for (n = 48; n < 58; n++)
+		{
 
-				putchar(n);
+			putchar(n);
 		
-			}
-			putchar('\n');
-			return (0);
+		}
+		putchar('\n');
+		return (0);
 }
