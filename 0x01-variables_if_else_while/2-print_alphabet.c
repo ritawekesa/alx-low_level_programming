0cf12d349dc86a538fdb62bade5eee636e9eb44b
ritawekesa <wekesaritah1@gmail>
2022-03-11 10:20:23 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,9 @@
  **/
 int main(void)
 {
-	int ch;
-		for (ch = 'a'; ch <= 'z'; ch++)
+		int ch;
+
+			for (ch = 'a'; ch <= 'z'; ch++)
 						putchar(ch);
 				for (ch = 'A'; ch <= 'Z'; ch++)
 							putchar(ch);
