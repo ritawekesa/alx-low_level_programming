79264824a832262ea738feb59f5a0949ab63d9a7
Wekesa Rita <wekesaritah1@gmail.com>
2022-03-22 19:21:58 +0300

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int len, n, m;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (m = len / 2; str[m] != '\0'; m++)
+		{
+			_putchar(str[m]);
+		}
+	}else if (len % 2 != 0)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}	
